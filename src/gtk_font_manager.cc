aa6b7b78a690221d9a34286ddbb16cb6730187b2
Garrick Meeker <garrick@digitalanarchy.com>
2022-02-12 11:48:04 -0800

    GTK compile errors

--- src/gtk_font_manager.cc
+++ src/gtk_font_manager.cc
@@ -2,11 +2,21 @@
 // #include <gtk/gtk.h>
 
 void ShowFontPanel(const Napi::CallbackInfo &info) {
-  // std::string title = info[0].As<Napi::String>().Utf8Value();
+  Napi::Env env = info.Env();
+  Napi::Function emit = info[0].As<Napi::Function>();
+  Napi::Object options = info[1].As<Napi::Object>();
+
+  std::string title;
+  Napi::Value titleValue = options.Get("title");
+  if (titleValue.IsString())
+      title = titleValue.As<Napi::String>().Utf8Value();
+
+  Napi::Promise::Deferred deferred = Napi::Promise::Deferred::New(env);
 
   // GtkFontChooser *fd = (GtkFontChooser*)gtk_font_chooser_dialog_new (title.c_str(), NULL);
 
   // gtk_widget_show(GTK_WIDGET(fd));
+
   return deferred.Promise();
 }
 
