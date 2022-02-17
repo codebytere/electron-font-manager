7d508b6ac509307615a4ee73312c50551e3bf1f4
Arthur <arthur.lang@epitech.eu>
2022-02-17 11:06:20 +0100

    use fontconfig instead, finish GetAvailableFontFamilies

--- src/gtk_font_manager.cc
+++ src/gtk_font_manager.cc
@@ -1,5 +1,9 @@
 #include <napi.h>
 #include <gtk/gtk.h>
+// #include <pango/pango.h>
+// #include <pango/pango-context.h>
+#include <fontconfig/fontconfig.h>
+#include <iostream> // TODO remove this
 
 /**
  * @brief ressources
@@ -15,22 +19,55 @@ void ShowFontPanel(const Napi::CallbackInfo &info) {
   gtk_widget_show(GTK_WIDGET(fd));
 }
 
-Napi::Array GetAvailableFonts(const Napi::CallbackInfo &info) {
-  // TODO Parameters gestion
+// trying with gtk
+// Napi::Array GetAvailableFonts(const Napi::CallbackInfo &info) {
+//   // TODO Parameters gestion
   
-  PangoContext *context = gtk_widget_create_pango_context(nullptr);
-  // retrieve fonts
-  PangoFontMap *fontmap = pango_context_get_font_map(context); // https://github.com/GNOME/pango/blob/main/pango/pango-fontmap.h
-  PangoFontFamily **fontFamilies;
-  int size = 0;
-  fontmap->pango_font_map_list_families(&fontFamilies, &size);
-  return {};
+//   PangoContext *context = pango_context_new();
+//   PangoFontMap 
+//   // retrieve fonts
+  
+//   PangoFontFamily **fontFamilies;
+//   int size = 0;
+//   context->pango_context_list_families(&fontFamilies, &size);
+//   std::cout << size << std::endl;
+//   for (int i = 0; i < size; i++) {
+//     std::cout << pango_font_family_get_name(fontFamilies[i]) << std::endl;
+//   }
+//   return {};
+// }
+
+// with fontconfig
+Napi::Array GetAvailableFontFamilies(const Napi::CallbackInfo &info) {
+  // TODO Parameters gestion
+  Napi::Env env = info.Env();
+
+  FcConfig *config = FcInitLoadConfigAndFonts();
+  FcPattern *pattern = FcPatternCreate();
+  FcObjectSet *set = FcObjectSetBuild(FC_FAMILY, FC_STYLE, FC_LANG, FC_FILE, (char *) 0);
+  // passing null here use the default configuration
+  FcFontSet *fontSet = FcFontList(config, pattern, set);
+
+  // init result
+  Napi::Array fontFamilies = Napi::Array::New(env, fontSet->nfont);
+  for (int i = 0; fontSet && i < fontSet->nfont; i++) {
+    FcPattern *currentPattern = fontSet->fonts[i];
+    FcChar8 *family;
+    if (FcPatternGetString(currentPattern, FC_FAMILY, 0, &family) == FcResultMatch) {
+      fontFamilies[i] = std::string(reinterpret_cast<const char*>(family)); // not sure cast is the best idea, return uchar string ?
+    }
+  }
+  return fontFamilies;
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
   exports.Set(
     Napi::String::New(env, "showFontPanel"), Napi::Function::New(env, ShowFontPanel)
   );
+  exports.Set(
+    Napi::String::New(env, "getAvailableFontFamilies"), Napi::Function::New(env, GetAvailableFontFamilies)
+  );
+  FcInit();
 
   return exports;
 }
